a3c85c2262a0f3ebe05bbb9e6479ff2406b5effc
SerhiiPavlichuk <S_Pavlichuk@icloud.com>
2021-10-12 07:02:23 +0300

    added button name

--- ReverseWords/ReverseWords/ViewController.swift
+++ ReverseWords/ReverseWords/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         textField.addTarget(self, action: #selector(reverseButtonPressed), for: .editingDidEnd)
        
     }
